eac5c1f017251758951ec46782a1dfd8f65144b3
blessingchisuwa <bchisuwa@buse.ac.zw>
2024-07-14 07:53:14 +0200

    Create 2024_07_14_055004_create_prepaid_transactions_table.php
    Create migration script for table prepaid_transactions

---
+++ database/migrations/2024_07_14_055004_create_prepaid_transactions_table.php
@@ -0,0 +1,41 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('prepaid_transactions', function (Blueprint $table) {
+            $table->id(); // id auto increment
+            $table->string('name', 255);
+            $table->string('phone', 255);
+            $table->integer('currency_id');
+            $table->double('rate', 15, 8);
+            $table->integer('transaction_type');
+            $table->double('amount', 15, 8);
+            $table->string('description', 255)->nullable();
+            $table->longText('notes')->nullable();
+            $table->integer('created_by');
+            $table->date('transaction_date');
+            $table->timestamps(); // created_at and updated_at timestamps
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('prepaid_transactions');
+    }
+};
