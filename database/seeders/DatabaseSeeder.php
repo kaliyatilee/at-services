233f855be927b56b2391532c7553021a35dcb9c7
John Mugabe <jonesmugabe08@hotmail.com>
2024-07-30 00:39:58 +0200

    Added Seeder

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -15,6 +15,7 @@ class DatabaseSeeder extends Seeder
      */
     public function run()
     {
+        $this->call(SystemChargesSeeder::class);
         $this->call(BulkAirtimeBalanceSeeder::class);
     }
 }
